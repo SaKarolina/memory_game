134d288b98ebca691bcb38d34b4d137835683cef
SaKarolina <salnaite.k@gmail.com>
2022-07-26 19:10:36 +0300

    Update design

--- app.js
+++ app.js
@@ -52,13 +52,15 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
-const cardsChosen = [];
-const cardsChosenIds = [];
+const resultDispaly = document.querySelector('#result');
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
 function createBoard () {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img');
-        card.setAttribute('src', 'images/blank.jpg');
+        card.setAttribute('src', 'images/blank5.png');
         card.setAttribute('data-id', i);
         card.addEventListener('click', flipCard);
         gridDisplay.appendChild(card);
@@ -67,13 +69,35 @@ function createBoard () {
 createBoard();
 
 function checkMatch() {
-    const cards = document.querySelectorAll('img')
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+
+    if (optionOneId === optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank5.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank5.png')
+        alert('You have clicked the same image!');
+    }
+
     if (cardsChosen[0] === cardsChosen[1]) {
         alert('You found a match!');
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
-        cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank5.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank5.png')
+        alert('Sorry try again!');
+    }
+    resultDispaly.textContent = cardsWon.length;
+
+    cardsChosen = [];
+    cardsChosenIds = [];
+
+    if (cardsWon.length === cardArray.length/2) {
+        resultDispaly.innerHTML = 'Congratulations! You found them all';
     }
 }
 
@@ -87,3 +111,6 @@ function flipCard () {
         setTimeout(checkMatch, 500);
     }
 }
+
+const year = document.querySelector('#current-year');
+year.innerHTML = new Date().getFullYear();
